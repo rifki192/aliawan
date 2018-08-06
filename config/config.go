00d9dfeeb8a963783dd1455fb3372a418cdd3168
Rifki <rifky_@outlook.com>
2018-08-06 18:06:51 +0700

    change env variable name

--- config/config.go
+++ config/config.go
@@ -8,9 +8,9 @@ import (
 
 // Config is defined all the configuration needed by the app
 type Config struct {
-	Region    string `env:"ALI_REGION" envDefault:"ap-southeast-1"`
-	AccesKey  string `env:"ALI_ACCESS_KEY"`
-	SecretKey string `env:"ALI_SECRET_KEY"`
+	Region    string `env:"ALICLOUD_REGION" envDefault:"ap-southeast-1"`
+	AccesKey  string `env:"ALICLOUD_ACCESS_KEY"`
+	SecretKey string `env:"ALICLOUD_SECRET_KEY"`
 }
 
 // LoadConfig for get all the configuration from Env Variables
